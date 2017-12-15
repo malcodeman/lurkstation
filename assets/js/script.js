18e3cb4c41c71afee3d1061e37b9bbafdf8cd474
malcodeman <malcodeman@users.noreply.github.com>
2017-12-16 00:46:38 +0100

    Add image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
+"use strict"
 const axios = require("axios");
 
+function addImage(source) {
+    let image = document.createElement("img");
+    image.src = source;
+    image.classList.add("image");
+    document.getElementById("main").appendChild(image);
+}
+
 function getPosts() {
-    axios.get("https://www.reddit.com/r/cats/top.json?limit=100")
+    axios.get("https://www.reddit.com/r/cats/top.json?limit=10")
         .then(response => {
             for (let i = 0; i < response.data.data.children.length; ++i) {
-                console.log(i + "  -  " + response.data.data.children[i].data.title);
+                let url = response.data.data.children[i].data.url;
+                console.log(i + "  -  " + url);
+                addImage(url);
             }
         })
         .catch(error => {
