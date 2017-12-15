5740c01369fb310bb94195f203daacf11f52fdf8
malcodeman <malcodeman@users.noreply.github.com>
2017-12-16 00:15:07 +0100

    get 100 posts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 const axios = require("axios");
 
-axios.get("https://www.reddit.com/r/Cyberpunk.json")
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+function getPosts() {
+    axios.get("https://www.reddit.com/r/cats/top.json?limit=100")
+        .then(response => {
+            for (let i = 0; i < response.data.data.children.length; ++i) {
+                console.log(i + "  -  " + response.data.data.children[i].data.title);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+getPosts();
