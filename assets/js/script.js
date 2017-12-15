af86257880f4dea4ea1bd37b0e0462900fe3e16d
malcodeman <malcodeman@users.noreply.github.com>
2017-12-15 22:43:21 +0100

    Added axios

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-alert("ready");
+const axios = require("axios");
+
+axios.get("https://www.reddit.com/r/Cyberpunk.json")
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
