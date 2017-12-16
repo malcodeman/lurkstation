15fa7c17283ae58b456ea5afd23e350ce7bc9795
malcodeman <malcodeman@users.noreply.github.com>
2017-12-16 20:16:56 +0100

    Grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@
 const axios = require("axios");
 
 function addImage(source) {
-    let image = document.createElement("img");
-    image.src = source;
+    let image = document.createElement("div");
+    image.style.backgroundImage = "url(" + source + ")";
     image.classList.add("image");
     document.getElementById("main").appendChild(image);
 }
 
 function getPosts() {
-    axios.get("https://www.reddit.com/r/cats/top.json?limit=10")
+    axios.get("https://www.reddit.com/r/art/new.json?limit=25")
         .then(response => {
             for (let i = 0; i < response.data.data.children.length; ++i) {
                 let url = response.data.data.children[i].data.url;
