1596c3fd7def68e64791e42eb54475b67a0d5d98
malcodeman <malcodeman@users.noreply.github.com>
2017-12-18 19:34:20 +0100

    Gfycat support

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,26 @@ const {
     remote
 } = require('electron')
 
-function addImage(source) {
-    let image = document.createElement("div");
-    image.style.backgroundImage = "url(" + source + ")";
+function renderImage(source) {
+    let post = document.createElement("div");
+    post.classList.add("post");
+    let image = document.createElement("img");
+    image.src = source;
     image.classList.add("image");
-    document.getElementById("main").appendChild(image);
+    post.appendChild(image);
+    document.getElementById("main").appendChild(post);
+}
+
+function renderVideo(source) {
+    let post = document.createElement("div");
+    post.classList.add("post");
+    let video = document.createElement("video");
+    video.src = source;
+    video.autoplay = "autoplay";
+    video.loop = "loop";
+    video.classList.add("video");
+    post.appendChild(video);
+    document.getElementById("main").appendChild(post);
 }
 
 function clearMain() {
@@ -16,7 +31,7 @@ function clearMain() {
 }
 
 function getPosts(choice) {
-    return axios.get("https://www.reddit.com/r/art/" + choice + ".json?limit=10")
+    return axios.get("https://www.reddit.com/r/pics/" + choice + ".json?limit=20")
         .then(response => {
             return response.data.data;
         })
@@ -49,16 +64,40 @@ function closeWindow() {
     remote.BrowserWindow.getFocusedWindow().close();
 }
 
+function getExtension(filename) {
+    return filename.split('.').pop();
+}
+
 function renderPosts(promise) {
     clearMain();
     promise.then(response => {
         for (let i = 0; i < response.children.length; ++i) {
+            let render = true;
             let url = response.children[i].data.url;
+            let extension = getExtension(url);
+            if (extension !== "png" && extension !== "jpg" && extension !== "gif") {
+                render = false;
+            }
             if (response.children[i].data.domain === "imgur.com") {
                 url += ".jpg";
             }
-            if (response.children[i].data.thumbnail !== "self") {
-                addImage(url);
+            if (response.children[i].data.domain === "flickr.com") {
+                render = false;
+            }
+            if (response.children[i].data.domain === "gfycat.com") {
+                // Adds 'giant' before and '.webm' after link
+                url = url.substr(0, 8) + "giant." + url.substr(8) + ".webm";
+                renderVideo(output);
+                render = false;
+            }
+            if (response.children[i].data.is_video === true) {
+                render = false;
+            }
+            if (response.children[i].data.thumbnail === "self") {
+                render = false;
+            }
+            if (render) {
+                renderImage(url);
             }
         }
     })
