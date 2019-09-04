5a1893763cb223090667408e040f121921015164
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-09-04 10:23:08 +0200

    Removes touchstart event listener from use on click outside hook

--- src/core/hooks/useOnClickOutside.js
+++ src/core/hooks/useOnClickOutside.js
@@ -10,10 +10,8 @@ function useOnClickOutside(ref, handler) {
     }
 
     document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
     return () => {
       document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
     };
   }, [ref, handler]);
 }
