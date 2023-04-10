fb148ed07964280a9e2925ff546126c8c8f47abb
malcodeman <26983729+malcodeman@users.noreply.github.com>
2023-04-10 14:01:33 +0200

    Updates post details page to use client side fetching

--- lurkstation/app/[subreddit]/comments/[id]/components/Post.tsx
+++ lurkstation/app/[subreddit]/comments/[id]/components/Post.tsx
@@ -3,6 +3,7 @@ import { Post } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import { useParams } from "next/navigation";
+import { length, map } from "ramda";
 
 const getComments = async (id: string) => {
   const response = await fetch(`/api/comments/${id}`);
@@ -16,27 +17,53 @@ export default function Post() {
     queryKey: ["post", id],
     queryFn: () => getComments(id),
   });
+  const post = data?.post.data;
+  const comments = data?.comments.children;
+  const mediaClassName = "w-full h-full object-cover md:object-contain";
 
-  const renderPost = (post: Post) => {
-    const { is_video, url } = post.data;
-
-    if (is_video) {
-      return (
-        <div className="relative w-full h-full overflow-y-hidden">
-          <video src={url} controls className="w-full h-full object-contain" />
-        </div>
-      );
-    }
-    return (
-      <div className="relative w-full h-full">
-        <Image src={url} alt="" fill className="w-full h-full object-contain" />
-      </div>
-    );
-  };
+  if (!post) {
+    return null;
+  }
 
   return (
-    <main className="mt-[46px] grid grid-cols-[1fr_365px] h-[calc(100vh_-_46px)]">
-      {data?.post ? renderPost(data.post) : null}
+    <main className="mt-[46px] md:grid md:gap-2 md:grid-cols-[1fr_365px] md:h-[calc(100vh_-_46px)]">
+      <div className="relative w-full h-full aspect-square overflow-y-hidden">
+        {post.is_video ? (
+          <video src={post.url} controls className={mediaClassName} />
+        ) : (
+          <Image
+            src={post.url}
+            alt=""
+            fill
+            priority
+            className={mediaClassName}
+          />
+        )}
+      </div>
+      <div className="p-2 overflow-y-auto">
+        <div className="mb-2">
+          <span className="text-xs">{post.author}</span>
+          <h1 className="text-xl">{post.title}</h1>
+          <span className="text-xs">{post.ups} ups</span>
+        </div>
+        <div>
+          <h2 className="text-l">{length(comments)} comments</h2>
+          <div>
+            {map(
+              (item) => (
+                <div key={item.data.id} className="mb-2">
+                  <span className="text-xs">{item.data.author}</span>
+                  <p className="text-sm break-words text-white/80">
+                    {item.data.body}
+                  </p>
+                  <span className="text-xs">{item.data.ups} ups</span>
+                </div>
+              ),
+              comments
+            )}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
