0aae7fab0808ec7493975649bee47ab3cd8b6e7b
malcodeman <26983729+malcodeman@users.noreply.github.com>
2023-04-15 16:32:17 +0200

    Fixes global error styling

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,11 +1,14 @@
 "use client";
 import ErrorComponent from "@/app/components/Error";
-import type { ErrorComponent as ErrorComponentType } from "@/types";
+import { type ErrorComponent as ErrorComponentType } from "@/types";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function GlobalError({ reset }: ErrorComponentType) {
   return (
-    <html>
-      <body>
+    <html lang="en" className={inter.className}>
+      <body className="bg-white text-gray-900 dark:bg-gray-900 dark:text-white">
         <ErrorComponent reset={reset} />
       </body>
     </html>
