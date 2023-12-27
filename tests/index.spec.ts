04b7e84e0693cde82fc944946684f96fcec68f65
malcodeman <26983729+malcodeman@users.noreply.github.com>
2023-12-28 00:53:39 +0100

    small fix

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -6,13 +6,14 @@ test.describe("home", () => {
   });
 
   test("Search input", async ({ page, baseURL }) => {
+    await page.waitForResponse("**/api/Art/hot");
+
     const input = page.getByTestId("subreddit-input");
 
     await input.fill("pics");
     await page.keyboard.press("Enter");
-
-    expect(input).toHaveValue("pics");
-    expect(page).toHaveURL(`${baseURL}/r/pics/hot`);
+    await expect(input).toHaveValue("pics");
+    await expect(page).toHaveURL(`${baseURL}/r/pics/hot`);
   });
 
   test("Time menu", async ({ page }) => {
@@ -20,73 +21,55 @@ test.describe("home", () => {
 
     const button = page.getByTestId("time-popover-button");
 
-    expect(button).toBeVisible();
+    await expect(button).toBeVisible();
   });
 
   test("Time menu - hour", async ({ page, baseURL }) => {
     await page.click("[data-testid=sort-top-link]");
     await page.click("[data-testid=time-popover-button]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
-
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
     await page.click("[data-testid=time-popover-panel-link-hour]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=hour`);
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=hour`);
   });
 
   test("Time menu - day", async ({ page, baseURL }) => {
     await page.click("[data-testid=sort-top-link]");
     await page.click("[data-testid=time-popover-button]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
-
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
     await page.click("[data-testid=time-popover-panel-link-day]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
   });
 
   test("Time menu - week", async ({ page, baseURL }) => {
     await page.click("[data-testid=sort-top-link]");
     await page.click("[data-testid=time-popover-button]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=week`);
-
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
     await page.click("[data-testid=time-popover-panel-link-week]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=week`);
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=week`);
   });
 
   test("Time menu - month", async ({ page, baseURL }) => {
     await page.click("[data-testid=sort-top-link]");
     await page.click("[data-testid=time-popover-button]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=month`);
-
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
     await page.click("[data-testid=time-popover-panel-link-month]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=month`);
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=month`);
   });
 
   test("Time menu - year", async ({ page, baseURL }) => {
     await page.click("[data-testid=sort-top-link]");
     await page.click("[data-testid=time-popover-button]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=year`);
-
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
     await page.click("[data-testid=time-popover-panel-link-year]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=year`);
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=year`);
   });
 
   test("Time menu - all", async ({ page, baseURL }) => {
     await page.click("[data-testid=sort-top-link]");
     await page.click("[data-testid=time-popover-button]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=all`);
-
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=day`);
     await page.click("[data-testid=time-popover-panel-link-all]");
-
-    expect(page).toHaveURL(`${baseURL}/r/Art/top?t=all`);
+    await expect(page).toHaveURL(`${baseURL}/r/Art/top?t=all`);
   });
 
   test("Go to first post", async ({ page, baseURL }) => {
@@ -96,8 +79,7 @@ test.describe("home", () => {
       .getAttribute("href");
 
     await page.locator("[data-testid=post]").first().click();
-
-    expect(page).toHaveURL(`${baseURL}${link}`);
+    await expect(page).toHaveURL(`${baseURL}${link}`);
   });
 
   test("Go to last post", async ({ page, baseURL }) => {
@@ -107,15 +89,13 @@ test.describe("home", () => {
       .getAttribute("href");
 
     await page.locator("[data-testid=post]").last().click();
-
-    expect(page).toHaveURL(`${baseURL}${link}`);
+    await expect(page).toHaveURL(`${baseURL}${link}`);
   });
 
   test("Post - x icon", async ({ page, baseURL }) => {
     await page.locator("[data-testid=post]").first().click();
     await page.click("[data-testid=x-icon]");
-
-    expect(page).toHaveURL(`${baseURL}/`);
+    await expect(page).toHaveURL(`${baseURL}/`);
   });
 
   test("Post - escape key", async ({ page, baseURL }) => {
@@ -125,11 +105,8 @@ test.describe("home", () => {
       .getAttribute("href");
 
     await page.locator("[data-testid=post]").first().click();
-
-    expect(page).toHaveURL(`${baseURL}${link}`);
-
+    await expect(page).toHaveURL(`${baseURL}${link}`);
     await page.keyboard.press("Escape");
-
-    expect(page).toHaveURL(`${baseURL}/`);
+    await expect(page).toHaveURL(`${baseURL}/`);
   });
 });
