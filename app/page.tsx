0ada889d6c6ccfe5db8a6a57bfd5ec080a65b9a3
malcodeman <26983729+malcodeman@users.noreply.github.com>
2024-05-17 18:00:31 +0200

    test hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
-"use client";
 import Posts from "@/app/components/Posts";
 import { DEFAULT_SORT, DEFAULT_SUBREDDIT } from "./lib/constants";
-import { useEffect } from "react";
 
 export default function App() {
   const subreddit = DEFAULT_SUBREDDIT;
   const sort = DEFAULT_SORT;
   const time = "";
   const queryKey = ["posts", subreddit, sort, time];
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`https://www.reddit.com/r/pics/top.json`);
-      const data = await response.json();
-      console.log(data);
-    };
-
-    fetchData();
-  }, []);
 
   return <Posts queryKey={queryKey} />;
 }
